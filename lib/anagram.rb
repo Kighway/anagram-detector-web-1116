1e15b6dec2462144b04cc409e9a2634709181d59
Kyle Tulod <tulod.kyle.m@gmail.com>
2016-12-05 14:49:06 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize (anagram)
+    @anagram = anagram
+  end
+
+  def match (possibilities)
+    sorted_anagram = anagram.split("").sort!
+    possibilities.select! { |possibility| possibility.split("").sort == sorted_anagram}
+  end
+end
